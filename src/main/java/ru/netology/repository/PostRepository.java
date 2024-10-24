0c7b76331bbf05e07a207e5905365512436a2ba2
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-10-24 22:18:26 +0300

    Конечная реализация

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,42 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong count = new AtomicLong();
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public Collection<Post> all() {
+        return posts.values();
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
 
-  public void removeById(long id) {
-  }
+    public Post save(Post post) {
+        if(post.getId() == 0){
+            long id= count.getAndIncrement();
+            post.setId(id);
+            posts.put(id, post);
+        } else if (posts.containsKey(post.getId())) {
+            posts.put(post.getId(), post);
+            }
+        else {
+            throw new NotFoundException("Post with id " + post.getId() + " not found");
+        }
+        return post;
+        }
+
+    public void removeById(long id) {
+        posts.remove(id);
+
+    }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -9,47 +9,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
+    private static final String  API_POSTS = "/api/posts";
+  private static final String  API_POSTSD = "/api/posts/\\d+";
+    private PostController controller;
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
     }
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals("GET") && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches(API_POSTSD)) {
+                // easy way
+                final var id = extractIdFromPath(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches(API_POSTSD)) {
+                // easy way
+                final var id = extractIdFromPath(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+  private long extractIdFromPath (String path){
+      return Long.parseLong(path.substring(path.lastIndexOf("/")));
   }
 }
 
